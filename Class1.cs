5a51cdc98934e24971bcf2ea6076c290d5a942b5
Andierrrr <123898346+Andierrrr@users.noreply.github.com>
2023-02-06 10:08:49 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿//Andy Allaico
+// Software Dev with Frameworks
+// CPS 3330 03
+// Unit 2 Lab assignment
+
+namespace Task5
+{
+    public class Class1
+    {
+        public static int CalculateProduct(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static int CalculateProduct(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Andy Allaico
+// Software Dev with Frameworks
+// CPS 3330 03
+// Unit 2 Lab assignment.
+using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount of integers you will give (2 or 3):");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            if (amount == 3)
+            {
+                Console.WriteLine("Enter the number for a:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number for b:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number for c:");
+                int c = Convert.ToInt32(Console.ReadLine());
+
+                int product = Task5.Class1.CalculateProduct(a, b, c);
+                Console.WriteLine($"The product of {a}, {b}, and {c} is {product}");
+            }
+            else
+            {
+                Console.WriteLine("Enter the number for a:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the number for b:");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int product = Task5.Class1.CalculateProduct(a, b);
+                Console.WriteLine($"The product of {a} and {b} is {product}");
+            }
+        }
+    }
+}
